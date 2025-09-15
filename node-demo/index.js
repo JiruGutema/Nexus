f9ea911d63a01d8ac86deb06094e79c9d352213e
JiruGutema <jirudagutema@gmail.com>
2025-09-15 21:04:01 +0300

    add: add node demo

---
+++ node-demo/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+// const chalk = require('chalk')
+
+console.log(chalk.green.bold('Hello world!'));
+console.log(chalk.bgYellow('Hello world!'));
+console.log(chalk.bgRed('Hello world!'));
