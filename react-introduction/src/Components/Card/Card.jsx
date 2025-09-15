956e49b49dff2bc63ef2e79ccbd89676f0157fec
JiruGutema <jirudagutema@gmail.com>
2025-09-15 21:02:41 +0300

    add: react introduction

---
+++ react-introduction/src/Components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import './Card.css'
+function Card(){
+    return <>
+    <div className='card'>
+        <p className='name'>Name: Random</p>
+        <p className='age'>Age: 20</p>
+    </div>
+      </>
+}
+
+export default Card
---
+++ react-introduction/src/Home/Home.jsx
@@ -0,0 +1,22 @@
+import Card from "../Components/Card/Card"
+import './Home.css'
+
+function Home() {
+
+  return (
+<div className="home">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+    
+
+  )
+}
+
+export default Home
+
